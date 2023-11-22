3af1afe3b18a182f26f7dc77e20b3f78af302895
FernandezCristian <fernandezcristian.utn@gmail.com>
2023-11-22 20:56:21 -0300

    filtrar producto 2

--- javaScript/script.js
+++ javaScript/script.js
@@ -52,15 +52,38 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let arrayProducto = ["lechuga", "banana", "azucar", "mango", "tucuca","ajo", "aceite", "tomate", "pure"];
 
 /**
  * Filtra la matríz en función de un criterio de búsqueda (query)
  */
-function filtrarProducto(query) {
-  return arrayProducto.filter(function (el) {
-    return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
+function filtrarProducto(productoABuscar) {
+  
+    return arrayProducto.filter(function (el) {
+    return el.toLowerCase().indexOf(productoABuscar.toLowerCase()) > -1;
   });
 }
 
-console.log(filtrarProducto("lech")); 
+console.log(filtrarProducto("az")); 
