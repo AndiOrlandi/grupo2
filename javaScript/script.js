2ab71f9056a78f7e8cb73e13a9b1a62106b94a21
Julian Martin <julianmartin19@outlook.com>
2023-11-22 20:18:31 -0300

    agregar productos al carrito

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,1 +1,18 @@
 //holi
+
+
+let carrito = [];
+
+cargarProductos();
+
+function cargarProductos (){
+    let n1 = parseInt(prompt("Digita la cantidad de elementos del array"))
+
+    for (let i = 0; i < n1; i++) {
+        parseInt(carrito.push(prompt(`Digita el elemento [${i + 1}] del array`)))
+    }
+    
+    console.log(`Tu carrito completo es: [${carrito}]`)
+
+}
+
