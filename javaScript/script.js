c576b99442542a2f272a3c590489da1f89dc02bf
Andi Orlandi <andijoorlandi>
2023-11-22 20:05:49 -0300

    commit3

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,1 +1,21 @@
 //holi
+/ Clase de Practica
+
+// 1.- Crear un repo en github.
+// 2.- Crear la rama dev. 
+// 3.- Vamos a tener solo un archivo index.html y un archivo script.js
+// 4.- Crear una rama por cada integrante del equipo.
+// 5.- Mergeamos las branch a dev.
+
+// Trabajo en equipo
+// Carrito de productos 
+// Carrito de compras con Array
+// ■ Crear una aplicación de carrito de compras donde se ingresen
+// productos como elementos en un array.
+// ■ las tareas que debe hacer son las siguientes:
+// ● Agregar producto al carrito
+// ● Listar los productos del carrito
+// ● Buscar un producto en el carrito
+// ● Filtrar productos que coincidan con una palabra, parte de ella
+// o una letra
+// ● Eliminar producto del carrito
