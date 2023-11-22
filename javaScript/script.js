446377979b036cdc31207bb36375245165d22bf6
FernandezCristian <fernandezcristian.utn@gmail.com>
2023-11-22 20:44:06 -0300

    filtrar producto

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,1 +1,66 @@
-//holi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let arrayProducto = ["lechuga", "banana", "azucar", "mango", "tucuca","ajo", "aceite", "tomate", "pure"];
+
+/**
+ * Filtra la matríz en función de un criterio de búsqueda (query)
+ */
+function filtrarProducto(query) {
+  return arrayProducto.filter(function (el) {
+    return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
+  });
+}
+
+console.log(filtrarProducto("lech")); 
