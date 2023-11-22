ba7598ccf1067f22ab93f049877d310ea61a96d2
ricardo amaya <ricardoamaya459@gmail.com>
2023-11-22 20:43:48 -0300

    buscar un elemento

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,1 +1,66 @@
-//holi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let array = ["carrito", 2, true, [1, 2, 3], { nombre: "carrito" }];
+ console.log(array[0]) 
+ console.log(array[1]) 
+ console.log(array[2]) 
+ console.log(array[3]) 
+ let carrito = [ 7, 8, 9, 9];
+
+let posicion = carrito.indexOf(9);
+console.log(posicion);
+
