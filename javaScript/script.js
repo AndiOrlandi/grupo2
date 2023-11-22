3b8bf72028867ff74bd19fd63cabf101c1256153
Facundo Gimenez <facuie014@gmail.com>
2023-11-22 20:33:41 -0300

    eliminar producto

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,1 +1,12 @@
-//holi
+
+arrayCarrito=["pelota","billetera","leche"]
+
+let eliminarProductos= ()=>{
+    let productoEliminado = prompt("que producto desea eliminar")
+    let index=arrayCarrito.indexOf(productoEliminado)
+    if(index!==-1){
+        arrayCarrito.splice(index, 1)
+    }
+    console.log(`el carrito queda asi: ${arrayCarrito}`)
+}
+eliminarProductos()
